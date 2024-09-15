3df07310c083e6b8841ba1d57d9457e65a4a96ac
nitinm96 <nitin.minhas@hotmail.com>
2024-09-14 21:28:02 -0400

    fix api url name

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv").config();
 
-const app = express(); 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 // Use the routes
-app.use('/api/articlesRoute', require('./routes/articlesRoute'));
-app.use('/api/users', require('./routes/usersRoute'));
+app.use("/api/articles", require("./routes/articlesRoute"));
+app.use("/api/users", require("./routes/usersRoute"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
